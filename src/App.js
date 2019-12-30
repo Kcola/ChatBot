0bc0b6460677b36052144ab88265995a14e9ede3
Kolawole Campbell <kcampbell@becht.com>
2019-12-30 13:01:53 -0500

    form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ChatWindow from "./components/ChatWindow";
 import ChatEntry from "./components/ChatEntry";
 function App() {
   const [Chatlog, enterChat] = useState([]);
+  const [SubmitFired, SetSubmitFired] = useState([]);
   const newChat = (newName, newChat) => {
+    SetSubmitFired(Chatlog.length);
     enterChat([
       ...Chatlog,
       {
@@ -16,20 +18,45 @@ function App() {
           new Date().getMinutes() +
           ":" +
           new Date().getSeconds()
-      },
-      {
-        key: Chatlog.length + 1,
-        name: "Bot",
-        chat: "You're Trash",
-        time:
-          new Date().getHours() +
-          ":" +
-          new Date().getMinutes() +
-          ":" +
-          new Date().getSeconds()
       }
     ]);
   };
+   useEffect(() => {
+     if(SubmitFired==0){
+      enterChat([
+        ...Chatlog,
+        {
+          key: Chatlog.length,
+          name: 'Bot',
+          chat: "Welcome to the chat room, I'm Bot",
+          time:
+            new Date().getHours() +
+            ":" +
+            new Date().getMinutes() +
+            ":" +
+            new Date().getSeconds()
+        }
+      ]);
+     }
+     else{
+       //This is where you I will call your API.
+      setTimeout(()=>{enterChat([
+        ...Chatlog,
+        {
+          key: Chatlog.length,
+          name: 'Bot',
+          chat: "Bot response",
+          time:
+            new Date().getHours() +
+            ":" +
+            new Date().getMinutes() +
+            ":" +
+            new Date().getSeconds()
+        }
+      ]);}, 2000) 
+     }
+    
+  }, [SubmitFired]) 
   return (
     <div>
       <ChatWindow Chatlog={Chatlog} />
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
 export default function ChatEntry({ newChat }) {
   const [chatVal, newChatVal] = useState([]);
+  const handleSubmit = event => {
+    event.preventDefault();
+    newChat("Kola", chatVal)
+  }
   return (
     <div>
-      <input onChange={event => newChatVal(event.target.value)}></input>
-      <button onClick={event => newChat("Kola", chatVal)}>Enter</button>
+      <form onSubmit={handleSubmit}>
+        <input onChange={event => newChatVal(event.target.value)}></input>
+        <input type="submit" value="Send"></input>
+      </form>
+
     </div>
   );
 }
