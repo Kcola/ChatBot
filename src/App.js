15b460ec2ab190aacfd1e99b1ca9bc6cc0622606
Kola Campbell <kcampbell@Kolas-MacBook-Pro.local>
2019-12-30 01:00:41 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ChatWindow from "./components/ChatWindow";
 
 function App() {
+  const [Chatlog, enterChat] = useState([]);
+  const [chatVal, newChatVal] = useState([]);
+  const newChat = (newName, newChat) => {
+    enterChat([
+      ...Chatlog,
+      {
+        name: newName,
+        chat: newChat,
+        time:
+          new Date().getHours() +
+          ":" +
+          new Date().getMinutes() +
+          ":" +
+          new Date().getSeconds()
+      },
+      {
+        name: 'Bot',
+        chat: "You're Trash",
+        time:
+          new Date().getHours() +
+          ":" +
+          new Date().getMinutes() +
+          ":" +
+          new Date().getSeconds()
+      }
+    ]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input onChange={event => newChatVal(event.target.value)}></input>
+      <button onClick={event => newChat('Kola', chatVal)}>Enter</button>
+        {Chatlog.map(chat => (
+          <p>{`${chat.time} ${chat.name} : ${chat.chat}`}</p>
+        ))}
     </div>
   );
 }
---
+++ src/components/ChatWindow.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ChatWindow() {
+  return (
+    <div>
+      <h1>Chat Winder</h1>
+    </div>
+  );
+}
+
+export default ChatWindow;
