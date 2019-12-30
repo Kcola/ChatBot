ffcc24e76b4e5432ff2450fb11e1ecc1703a0e1f
Kolawole Campbell <kcampbell@becht.com>
2019-12-30 14:29:54 -0500

    Updates

---
+++ precache-manifest.b7965589a08f027bacb48105f8be4d74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afad0dee3a16078c5adcf8059c1873c3",
+    "url": "/ChatBot/index.html"
+  },
+  {
+    "revision": "36b7bb0f30984d903123",
+    "url": "/ChatBot/static/js/2.3010e219.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ChatBot/static/js/2.3010e219.chunk.js.LICENSE"
+  },
+  {
+    "revision": "448378205459c792edf8",
+    "url": "/ChatBot/static/js/main.037d3cdc.chunk.js"
+  },
+  {
+    "revision": "8dc500d8d57bd431fd68",
+    "url": "/ChatBot/static/js/runtime-main.a61db72a.js"
+  }
+]);
